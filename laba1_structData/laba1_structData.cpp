ee72338edec39b06d022fcc55197f2f6b366eefc
Skrippe3 <97904563+Skrippe3@users.noreply.github.com>
2024-03-01 16:30:09 +0300

    Update laba1_structData.cpp

--- laba1_structData/laba1_structData.cpp
+++ laba1_structData/laba1_structData.cpp
@@ -8,7 +8,7 @@ long long gcd(long long a, long long b, long long& x, long long& y) {
         return b;
     }
     long long x1, y1;
-    int d = gcd(b % a, a, x1, y1);
+    long long d = gcd(b % a, a, x1, y1);
     x = y1 - (b / a) * x1;
     y = x1;
     return d;
